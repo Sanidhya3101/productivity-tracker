cc860a9eeb93da65ae056370ba3ae793941b48f3
Aryaman <aryaman13jan@gmail.com>
2024-11-19 23:25:52 +0530

    Completed the creative task -- writing mail

---
+++ src/pages/textarea.js
@@ -0,0 +1,9 @@
+export default function NewPost() {
+    return (
+      <label>
+        Write your post:
+        <textarea name="postContent" rows={4} cols={40} />
+      </label>
+    );
+  }
+  
