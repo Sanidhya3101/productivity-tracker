06a6ee8837558e8ec2ba193aa358394e0df4cf62
Aryaman <aryaman13jan@gmail.com>
2024-11-19 23:29:45 +0530

    Removed textarea, added by mistake in the pages folder.

--- src/pages/textarea.js
+++
@@ -1,9 +0,0 @@
-export default function NewPost() {
-    return (
-      <label>
-        Write your post:
-        <textarea name="postContent" rows={4} cols={40} />
-      </label>
-    );
-  }
-  
